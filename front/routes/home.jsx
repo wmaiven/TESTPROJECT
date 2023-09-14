12169938bab5580a877c930f69487175d9ca55b9
wmaiven <maivencunha@hotmail.com>
2023-09-14 12:23:16 -0300

    pagina home e style criada

--- front/routes/home.jsx
+++ front/routes/home.jsx
@@ -1,9 +1,24 @@
-
+import "./home.css"
 const Home = () => {
   return (
-    <div>
-        <h1>home</h1>
-
+    <div class="container">
+        <h1>Bem-vindo à minha aplicação</h1>
+        <div class="box">
+            <h2>Desafio 1: Números Palíndromos</h2>
+            <p>Para encontrar números palíndromos, clique <a href="./palindromo">aqui</a>.</p>
+        </div>
+        <div class="box">
+            <h2>Desafio 2: Troco em Notas</h2>
+            <p>Para calcular o troco em notas, clique <a href="./Caixa">aqui</a>.</p>
+        </div>
+        <div class="box">
+            <h2>Desafio 3: Gerenciamento de Veículos</h2>
+            <p>Para gerenciar veículos, clique <a href="./CadastroVeiculo">aqui</a>.</p>
+        </div>
+        <div class="box">
+            <h2>Desafio 4: Consumir API VIACep</h2>
+            <p>Para descobrir informações sobre algum CEP, clique <a href="./ConsultaCep">aqui</a>.</p>
+        </div>
     </div>
   )
 }
